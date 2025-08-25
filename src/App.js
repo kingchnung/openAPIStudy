e731a9984183c604c0a00a099bc87cbff15f91af
kingchnung <cksdnd1232@gmail.com>
2025-08-25 17:09:03 +0900

    08.25

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
-const url = 'http://apis.data.go.kr/6480000/gyeongnammuseum/gyeongnammuseumList?serviceKey=6bbab4fdce6620ecc007908138791a2affdd5890dbd90bc585d0e79c91a2c9fd&pageNo=1&numOfRows=4&resultType=json';
+const url = 'http://apis.data.go.kr/6260000/WalkingService/getWalkingKr?serviceKey=6bbab4fdce6620ecc007908138791a2affdd5890dbd90bc585d0e79c91a2c9fd&numOfRows=10&pageNo=1&resultType=json';
 
 
 function App() {
@@ -10,37 +12,49 @@ function App() {
 
   useEffect(()=> {
     fetch(url).then(response => response.json()).then(data => {
-      setResult(data.gyeongnammuseumList.body.items.item)
+      setResult(data.getWalkingKr.item)
     }).catch(error => console.log(error));
   }, []);
 
   return (
     <div className="App">
-      <h3>경산남도_박물관 정보</h3>
+      <h3>부산 걷기</h3>
       <table className='App-table'>
         <thead>
           <tr>
-            <th>고유번호</th>
-            <th>기관명</th>
-            <th>소재지</th>
-            <th>홈페이지</th>
-            <th>연락처</th>
+            <th>NO</th>
+            <th>TITLE</th>
+            <th>LAT</th>
+            <th>LNG</th>
+            <th>PLACE</th>
           </tr>
         </thead>
         <tbody>
           {
             result.map(item => (
-              <tr key={item.entid}>
-                <td>{item.entid}</td>
-                <td>{item.title}</td>
-                <td>{item.roadaddress}</td>
-                <td>{item.homepage}</td>
-                <td>{item.tel}</td>
+              <tr key={item.UC_SEQ}>
+                <td>{item.UC_SEQ}</td>
+                <td>{item.TITLE}</td>
+                <td>{item.LAT}</td>
+                <td>{item.LNG}</td>
+                <td>{item.PLACE}</td>
               </tr>
             ))
           }
         </tbody>
       </table>
+      <div className='btns'>
+      <Button variant="success" className='btn'>Success</Button>
+      <Button variant="primary" className='btn'>Primary</Button>
+      <Button variant="secondary" className='btn'>Secondary</Button>
+      <Button variant="success" className='btn'>Success</Button>
+      <Button variant="warning" className='btn'>Warning</Button>
+      <Button variant="danger" className='btn'>Danger</Button>
+      <Button variant="info" className='btn'>Info</Button>
+      <Button variant="light" className='btn'>Light</Button>
+      <Button variant="dark" className='btn'>Dark</Button>
+      <Button variant="link" className='btn'>Link</Button>
+      </div>
     </div>
   );
 };
